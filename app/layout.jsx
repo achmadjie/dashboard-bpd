ee55dd5a02ce6674dddab303a9fc092567616f3a
Andi Adjie <achmdadjie@gmail.com>
2023-09-15 15:22:13 +0700

    change the title and description of the project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,16 @@
 // import LoginForm from './components/login-form/LoginForm'
-import 'bootstrap/dist/css/bootstrap.css'
-import Login from './pages/login/Login'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "bootstrap/dist/css/bootstrap.css";
+import Login from "./pages/login/Login";
+import SideBar from "./components/side-bar/SideBar";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Dashboard BPD',
-  description: 'Dashboard BPD',
-}
+  title: "Dashboard BPD",
+  description: "Dashboard BPD",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -17,8 +18,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {/* {children} */}
         {/* <LoginForm /> */}
-        <Login />
+        {/* <Login /> */}
+        <SideBar />
       </body>
     </html>
-  )
+  );
 }
